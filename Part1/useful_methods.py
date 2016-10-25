b93f81989771d849d62c964f3c6b99aaa58e9a6a
Joakim Bengtsson <joakim.c.bengtsson@gmail.com>
2016-10-25 21:31:15 +0200

    update

--- Part1/useful_methods.py
+++ Part1/useful_methods.py
@@ -16,5 +16,30 @@ import random
 # print(userInput)
 
 ##Random number
-number = random.randint(1, 20)
-print(number)
+# number = random.randint(1, 20)
+# print(number)
+
+##Regex example
+# import re
+# isAMatch = re.match("^[0-9]*$", "10d")
+# if isAMatch:
+#     print("is a match!")
+# else:
+#     print("not a match :(")
+
+##Run OS commands
+# import os
+
+## Print windows routing table
+# os.system('route print')
+
+## Clear terminal cross-platform example
+# os.system('cls' if os.name == 'nt' else 'clear')
+
+##Exception handling example
+# notAnInt = "1"
+# try:
+#     int(notAnInt)
+#     print("Converted the string to an int!")
+# except ValueError:
+#     print("String is not an int! :(")
